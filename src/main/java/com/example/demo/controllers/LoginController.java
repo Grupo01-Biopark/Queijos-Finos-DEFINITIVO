8f4c9521b0ef4e0efbea10b80b66daaf5a3957f0
Kelvin de Jesus Oliveira <kelvin.oliveira@alunos.bpkedu.com.br>
2024-04-17 21:50:04 -0300

    desenvolvimento de controller e service(falhou ajustar ambos), adição de um arquivo teste (testUser.sql) para testar funcionamento de login

--- src/main/java/com/example/demo/controllers/LoginController.java
+++ src/main/java/com/example/demo/controllers/LoginController.java
@@ -1,16 +1,43 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.service.LoginService;
 
 @Controller
 public class LoginController {
 
+	@Autowired
+    private LoginService loginService;	
+	
 	@GetMapping("/Login")
 	public ModelAndView Login() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("Login/Login");
 		return mv;
 	}
+	
+	@PostMapping("/login")
+public String login(@RequestParam("email") String email,
+                    @RequestParam("password") String password,
+                    RedirectAttributes redirectAttributes) {
+    System.out.println("Email recebido no controller: " + email);
+    System.out.println("Senha recebida no controller: " + password);
+    boolean loginSuccessful = loginService.checkLogin(email, password);
+    if (loginSuccessful) {
+        return "redirect:/home"; 
+    } else {
+        redirectAttributes.addFlashAttribute("error", "Credenciais inválidas. Por favor, tente novamente.");
+        return "redirect:/PasswordReset"; 
+    }
+}
+
+	
+	
 }
--- src/main/java/com/example/demo/service/LoginService.java
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -5,11 +5,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoginService {
 
-	public String login(String email, String password) {
-		
-		
-		
-		return "token";
-	}
-	
+    public String login(String email, String password) {
+        System.out.println("Email recebido no serviço: " + email);
+        System.out.println("Senha recebida no serviço: " + password);
+        return "token";
+    }
+
+    public boolean checkLogin(String email, String password) {
+        System.out.println("Email recebido no serviço para verificação: " + email);
+        System.out.println("Senha recebida no serviço para verificação: " + password);
+        return false;
+    }
 }
+
