e5ea9dbc9b06223ae6934a93985158c97b3eccc1
Kelvin Oliveira <kelvin.oliveira.kjo@gmail.com>
2024-04-21 22:42:03 -0300

    versão 2 tentativa de desenvolvimento de controller e repository (falhou)

--- src/main/java/com/example/demo/controllers/LoginController.java
+++ src/main/java/com/example/demo/controllers/LoginController.java
@@ -1,16 +1,38 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import ch.qos.logback.core.model.Model;
 
 @Controller
 public class LoginController {
 
-	@GetMapping("/Login")
-	public ModelAndView Login() {
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("Login/Login");
-		return mv;
-	}
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping("/Login")
+    public ModelAndView login() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("login/login");
+        return mv; 
+    }
+
+    @PostMapping("/logInto")
+    public String logar(Model model, @RequestParam("email") String email, @RequestParam("password") String password) {
+        User user = repository.login(email, password);
+
+        if (user != null) {
+            return "redirect:/home";
+        }
+
+        return "redirect:/erro";
+    }
 }
+
+
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_user (name_user, email, password, tipo_user_permission)
+VALUES ('Alice', 'alice@example.com', 'password123', 'ADMIN');
+
+INSERT INTO tb_user (name_user, email, password, tipo_user_permission)
+VALUES ('Bob', 'bob@example.com', 'password456', 'USER');
