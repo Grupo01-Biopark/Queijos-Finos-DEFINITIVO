d6aef8c1934ca257def91bf8a643f496cb72cbb0
Kelvin Oliveira <kelvin.oliveira.kjo@gmail.com>
2024-04-23 23:47:55 -0300

    remoção de testes sql

--- src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tb_user (name_user, email, password, tipo_user_permission)
-VALUES ('Alice', 'alice@example.com', 'password123', 'ADMIN');
-
-INSERT INTO tb_user (name_user, email, password, tipo_user_permission)
-VALUES ('Bob', 'bob@example.com', 'password456', 'USER');
--- src/main/resources/testUser.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tb_user (name_user, email, password, tipo_user_permission, cpf, cnpj, razao_social, obs, status)
-VALUES ('João Silva', 'joao.silva@example.com', 'senha123', 'ADMIN', '123.456.789-00', NULL, NULL, 'Usuário administrativo', 'ATIVO');
-
-INSERT INTO tb_user (name_user, email, password, tipo_user_permission, cpf, cnpj, razao_social, obs, status)
-VALUES ('Maria Oliveira', 'maria.oliveira@example.com', 'senha456', 'CLIENTE', NULL, '12.345.678/0001-90', 'Empresa Exemplo', 'Cliente comum', 'INATIVO');
