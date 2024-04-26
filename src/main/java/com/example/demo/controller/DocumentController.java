a3b44250ff4bf1c15ed112edcd021af33cdeabe6
Vinicius Eduardo Agnes <114628323+ViniciusAgnes@users.noreply.github.com>
2024-04-26 19:58:50 -0300

    Mapping

--- src/main/java/com/example/demo/controller/DocumentController.java
+++ src/main/java/com/example/demo/controller/DocumentController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,31 +15,48 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.example.demo.entity.Document;
+import com.example.demo.service.DocumentService;
 
+@RestController
 public class DocumentController {
-    
-}
 
-@GetMapping("/cadastro")
-public ModelAndView seuMetodo() {
-    User user = new User();
-    
-    ModelAndView modelAndView = new ModelAndView();
-    modelAndView.setViewName("cadastroUsuario"); // Define o nome da visualização que você deseja retornar
-    modelAndView.addObject("user", user);
-    // Adicionar a lista de usuários ao modelo
-    modelAndView.addObject("users", getAllUsers());
+    @Autowired
+    private DocumentService DocumentService;
 
-    return modelAndView;
-}
+    @GetMapping("/Documentos")
+    public ModelAndView iniciarTela() {
+        Document Document = new Document();
 
-@PostMapping("/cadastro/usuario")
-public RedirectView createUser(@ModelAttribute("user") User user, RedirectAttributes attributes) {
-    try {
-        userService.creatUser(user);
-        attributes.addFlashAttribute("condition", "condition");
-    } catch (DataIntegrityViolationException e) {
-        attributes.addFlashAttribute("mensagem", "Email ja cadastardo no sistema "+e.getMessage());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("gerenciamentoDocumentos");
+        modelAndView.addObject("Document", Document);
+       // modelAndView.addObject("technologies", getAllTechnologies());
+
+        return modelAndView;
     }
-    return new RedirectView("/cadastro");
+
+    @PostMapping("/Documentos/cadastrar")
+    public RedirectView createDocument(@ModelAttribute("Document") Document Document, RedirectAttributes attributes) {
+        try {
+            DocumentService.createDocument(Document);
+            attributes.addFlashAttribute("mensagem", "Documento adicionado com sucesso");
+        } catch (DataIntegrityViolationException e) {
+            attributes.addFlashAttribute("mensagem", "Erro ao adicionar o documento: " + e.getMessage());
+        }
+        return new RedirectView("/Documentos");
+    }
+
+    public List<Document> getAllTechnologies() {
+        List<Document> technologies = DocumentService.getListDocument();
+        return technologies;
+        
+    }
+
+    @DeleteMapping("/technologies/{DocumentId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long DocumentID) {
+        DocumentService.deleteDocument(DocumentID);
+        return ResponseEntity.ok().build();
+    }
+
 }
