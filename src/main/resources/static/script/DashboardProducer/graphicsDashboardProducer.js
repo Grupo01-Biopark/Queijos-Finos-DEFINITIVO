27d39201489182eb2a7540d05762088fbb9f66d9
Kelvin de Jesus Oliveira <kelvin.oliveira@alunos.bpkedu.com.br>
2024-04-29 22:16:45 -0300

    estilização dos graficos

--- src/main/resources/static/script/DashboardProducer/graphicsDashboardProducer.js
+++ src/main/resources/static/script/DashboardProducer/graphicsDashboardProducer.js
@@ -1,48 +1,94 @@
 const ProductionStatus = document.getElementById('graphicProductionStatus');
+
 const config1 = {
+
   type: 'doughnut',
+
   data: {
-    labels: ['Ativos Produzindo', 'Ativos em complementação', 'Desistentes','Desligados'],
+
+    labels: ['Ativos Produzindo', 'Ativos em complementação', 'Desistentes', 'Desligados'],
 
     datasets: [{
-      label: '',
-      data: [300, 50, 30,70],
-      backgroundColor: 
-      ['rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)',
-      'rgb(255, 205, 86)',
-      'rgb(38, 35, 30)'],
 
-      hoverOffset: 5
+      label: '',
+
+      data: [83, 47, 25, 10],
+
+      backgroundColor: [
+
+        '#059bff',
+        '#ffc234',
+        '#ff4069',
+        '#9a9b98'
+
+      ],
+
+      hoverOffset: 4
       
     }]
-  }
+  },
+
+  options: {
+    plugins: {
+        legend: {
+            labels: {
+
+              font: {
+                family: 'Poppins', 
+                size: 16, 
+                style: 'normal'
+            },
+                
+            }
+        }
+    }
+}
+
 };
 new Chart(ProductionStatus, config1);
+
+
 
 const certificationsAchieveds = document.getElementById('graphicCertificationsAchieveds');
                   
 new Chart(certificationsAchieveds, {
 
     type: 'bar',
+
     data: {
+
     labels: ['Não Possui', 'Sim/POA', 'SUSAF', 'SISBI', 'SELO ARTE', 'SIF'],
+
     datasets: [{
-        maxBarThickness: 100,
-    label: '',
+
+    maxBarThickness: 100,
+
+    label: 'false', //remover aparencia da lable
+
     data: [55, 77, 30, 25, 25, 3],
+
     borderWidth: 1
-    }]
-    },
+    
+    }]},
+
     options: {
 
     scales: {
 
-        y: {
-            
+      y: {
+        grid: {
+            display: false // Remove as linhas de grade do eixo Y
+        },
+        beginAtZero: true
+    },
+    x: {
+        grid: {
+            display: false // Remove as linhas de grade do eixo X
+        }
+    },
+    
         beginAtZero: true
         }
     }
     }
-    });                      
+    );                      
