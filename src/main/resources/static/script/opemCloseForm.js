0c4b99bd187fc29fb37c62d32a0e9975cfcaf67f
Matheus Morilha <matheusmorilha04@gmail.com>
2024-05-02 20:43:33 -0300

    Modificações feitas no arquivo opemCloseForm definindo o endpoint do form via script

--- src/main/resources/static/script/opemCloseForm.js
+++ src/main/resources/static/script/opemCloseForm.js
@@ -1,25 +1,34 @@
 const openFormButton = document.getElementById("buttonAdd");
 const closeFormButton = document.getElementById("buttonCancel");
+const sectionForm = document.getElementById("sectionForm");
 
-openFormButton.addEventListener("click", () => {
-    const sectionForm = document.getElementById("sectionForm");
-    const formAction = sectionForm.querySelector("form");
+const formValuesUsers = {
+    action: "/users/register",
+    method: "post",
+    title: "Novo usuário"
+};
 
-    formAction.setAttribute("method", "post");
-    formAction.setAttribute("action", "/users/register");
-    sectionForm.querySelector(".titleForm").textContent = "Novo usuário";
-    sectionForm.style.display = "block";
-    openFormButton.style.display = "none";
+const formValuesTechnologies = {
+    action: "/tecnologias/cadastrar",
+    method: "post",
+    title: "Nova tecnologia"
+};
+
+openFormButton.addEventListener("click", () => {  
+    const page = openFormButton.getAttribute("data-page");
+    const formValues =  page === "technology" ? formValuesTechnologies : formValuesUsers;
+    opemForm(formValues);
 });
+
 
 closeFormButton.addEventListener("click", () => {
     const sectionForm = document.getElementById("sectionForm");
     const alertPopupForm = document.getElementById("popupAlertForm");
     const sectionPopup = document.querySelector(".sectionPopup");
-
+    
     sectionPopup.style.display = "block";
     alertPopupForm.style.display = "block";
-
+    
     const exitButton = document.getElementById("buttonPopupAlertExit").addEventListener("click", () => {
         sectionForm.style.display = "none";
         openFormButton.style.display = "block";
@@ -27,6 +36,17 @@ closeFormButton.addEventListener("click", () => {
         clearFormFields();
     });
 });
+
+function opemForm(formValues){
+    const formAction = sectionForm.querySelector("form");
+
+    formAction.setAttribute("method", formValues.method);
+    formAction.setAttribute("action", formValues.action);
+    sectionForm.querySelector(".titleForm").textContent = formValues.title;
+
+    sectionForm.style.display = "block";
+    openFormButton.style.display = "none";
+}
 
 function clearFormFields() {
     const formInputs = document.querySelectorAll("#sectionForm input");
