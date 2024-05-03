9a7f29679f6836b142c7406bf14c18d94429b0bf
Vinicius Eduardo Agnes <114628323+ViniciusAgnes@users.noreply.github.com>
2024-05-02 21:55:55 -0300

    ATT CSS

---
+++ src/main/resources/static/script/AdicionarPopupDocumento.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();  
+
+        const nome = document.getElementById('name').value;
+        const dataDeUpload = document.getElementById('Datadeupload').value;
+        const tipoDeDocumento = document.getElementById('TipoDocumento').value;
+        const dataDeVencimento = document.getElementById('Data de Vencimento').value;
+        const arquivoCarregado = document.getElementById('fileUpload').files[0];
+
+
+        if (!nome || !dataDeUpload || !tipoDeDocumento || !dataDeVencimento || !arquivoCarregado) {
+            alert('Por favor, preencha todos os campos e adicione um documento.');
+            return;
+        }
+
+        console.log('Nome:', nome);
+        console.log('Data de Upload:', dataDeUpload);
+        console.log('Tipo de Documento:', tipoDeDocumento);
+        console.log('Data de Vencimento:', dataDeVencimento);
+        console.log('Documento:', arquivoCarregado.name);
+
+        });
+});
