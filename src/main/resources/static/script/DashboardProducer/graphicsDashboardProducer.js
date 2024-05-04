91ebba91b666ac39718bebb07480b4ce166d215c
Kelvin Oliveira <kelvin.oliveira.kjo@gmail.com>
2024-05-04 19:57:45 -0300

    estilização dos graficos status de produção e certificações conquistadas, desenvolvimento FrontEnd do grafico tecnologias

--- src/main/resources/static/script/DashboardProducer/graphicsDashboardProducer.js
+++ src/main/resources/static/script/DashboardProducer/graphicsDashboardProducer.js
@@ -12,7 +12,7 @@ const config1 = {
 
       label: '',
 
-      data: [83, 47, 25, 10],
+      data: [83, 47, 25, 4],
 
       backgroundColor: [
 
@@ -36,12 +36,11 @@ const config1 = {
               font: {
                 family: 'Poppins', 
                 size: 16, 
-                style: 'normal'
             },
                 
             }
         }
-    }
+    } 
 }
 
 };
@@ -50,45 +49,113 @@ new Chart(ProductionStatus, config1);
 
 
 const certificationsAchieveds = document.getElementById('graphicCertificationsAchieveds');
-                  
+
 new Chart(certificationsAchieveds, {
+type: 'bar',
 
-    type: 'bar',
+data: {
+  labels: ['Não Possui', 'Sim/POA', 'SUSAF', 'SISBI', 'SELO ARTE', 'SIF'],
+  datasets: [{
+      maxBarThickness: 100,
+      label: ' ', // remover aparência da label
+      data: [55, 77, 30, 25, 25, 3],
+      backgroundColor: ['rgba(75, 192, 192, 0.6)'],
+      borderColor: ['rgba(75, 192, 192, 10)'],
+      borderWidth: 4
+  }]
+},
 
-    data: {
+options: {
 
-    labels: ['Não Possui', 'Sim/POA', 'SUSAF', 'SISBI', 'SELO ARTE', 'SIF'],
+  scales: {
 
-    datasets: [{
-
-    maxBarThickness: 100,
-
-    label: 'false', //remover aparencia da lable
-
-    data: [55, 77, 30, 25, 25, 3],
-
-    borderWidth: 1
-    
-    }]},
-
-    options: {
-
-    scales: {
-
-      y: {
+    y: {
+        ticks: {
+            font: {
+                family: 'Poppins', // Alterar aqui a família da fonte
+                size: 16 // Alterar aqui o tamanho da fonte
+            }
+        },
         grid: {
-            display: false // Remove as linhas de grade do eixo Y
+            display: true, // ativa as linhas de grade do eixo Y
+            lineWidth: 1
         },
         beginAtZero: true
     },
+
     x: {
+        ticks: {
+            font: {
+                family: 'Poppins', // Alterar aqui a família da fonte
+                size: 16 // Alterar aqui o tamanho da fonte
+            }
+        },
         grid: {
-            display: false // Remove as linhas de grade do eixo X
+            display: true, // ativa as linhas de grade do eixo X
+            lineWidth: 1
         }
+    }
+},
+
+beginAtZero: true
+
+}
+});
+
+const technologies = document.getElementById('technologies');
+
+new Chart(technologies, {
+    type: 'bar',
+    data: {
+        labels: ['Canastra', 'Minas', 'Brie', 'Cheddar', 'Gouda', 'Mussarela'],
+        datasets: [{
+            label: 'Produzindo',
+            backgroundColor: 'rgb(5, 155, 255, 0.6)',
+            borderColor: 'rgb(5, 155, 255, 10)',
+            borderWidth: 4,
+            data: [55, 77, 30, 25, 25, 3]
+        }, {
+            label: 'Em complementação',
+            backgroundColor: 'rgb(255, 194, 52, 0.6)',
+            borderColor: 'rgb(255, 194, 52)',
+            borderWidth: 4,
+            data: [35, 50, 20, 15, 30, 5]
+        }, {
+            label: 'Desistentes',
+            backgroundColor: 'rgb(255, 64, 105, 0.6)',
+            borderColor: 'rgb(255, 64, 105, 10)',
+            borderWidth: 4,
+            data: [20, 30, 10, 10, 20, 2]
+        }]
     },
-    
+    options: {
+        scales: {
+            y: {
+                ticks: {
+                    font: {
+                        family: 'Poppins',
+                        size: 16
+                    }
+                },
+                grid: {
+                    display: true,
+                    lineWidth: 1
+                },
+                beginAtZero: true
+            },
+            x: {
+                ticks: {
+                    font: {
+                        family: 'Poppins',
+                        size: 16
+                    }
+                },
+                grid: {
+                    display: true,
+                    lineWidth: 1
+                }
+            }
+        },
         beginAtZero: true
-        }
     }
-    }
-    );                      
+});
