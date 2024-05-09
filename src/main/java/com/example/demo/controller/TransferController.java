f3be183bd2a9c402bbd2e0b468dc671a218868f0
Matheus Morilha <matheusmorilha04@gmail.com>
2024-05-09 20:42:37 -0300

    Implementando a lógica para puxar todas as tecnologias existentes no banco de dados, para dentro do Select do formulário.

--- src/main/java/com/example/demo/controller/TransferController.java
+++ src/main/java/com/example/demo/controller/TransferController.java
@@ -1,13 +1,11 @@
 package com.example.demo.controller;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
+
 import com.example.demo.entity.Transfer;
+import com.example.demo.service.TechnologyService;
 
 
 
@@ -15,29 +13,31 @@ import com.example.demo.entity.Transfer;
 @RestController
 public class TransferController {
 
-
+    @Autowired
+    TechnologyService technologyService;
 
     @GetMapping("/transfer")
-    public ModelAndView showscreen(){
+    public ModelAndView showscreen() {
         Transfer transfer = new Transfer();
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("gerenciamentoTransferencias");
         modelAndView.addObject("transfer", transfer);
-
+        modelAndView.addObject("technologies", technologyService.getListTechnology());
         return modelAndView;
     }
+    
 
-     @PostMapping("/transfer/register")
-    public RedirectView registerTransfer(@ModelAttribute("transfer") Transfer transfer, RedirectAttributes attributes) {
-        try {
-            System.out.println("Hello World");
-            // technologyService.createTechnology(technology);
-            attributes.addFlashAttribute("condition", "condition");
-        } catch (DataIntegrityViolationException e) {
-            attributes.addFlashAttribute("mensagem", "Erro ao cadastrar tecnologia: " + e.getMessage());
-        }
-        return new RedirectView("/transfer");
-    }
+    //  @PostMapping("/transfer/register")
+    // public RedirectView registerTransfer(@ModelAttribute("transfer") Transfer transfer, RedirectAttributes attributes) {
+    //     try {
+    //         System.out.println("Hello World");
+    //         // technologyService.createTechnology(technology);
+    //         attributes.addFlashAttribute("condition", "condition");
+    //     } catch (DataIntegrityViolationException e) {
+    //         attributes.addFlashAttribute("mensagem", "Erro ao cadastrar tecnologia: " + e.getMessage());
+    //     }
+    //     return new RedirectView("/transfer");
+    // }
 
     
 }
--- src/main/java/com/example/demo/service/TransferService.java
+++ src/main/java/com/example/demo/service/TransferService.java
@@ -1,20 +1,10 @@
 package com.example.demo.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.Transfer;
-import com.example.demo.repository.TransferRepository;
 
-import jakarta.validation.Valid;
 
 @Service
 public class TransferService {
 
-    // @Autowired
-    // private TransferRepository transferRepository;
 
-    // public Transfer registerTransfer(@Valid Transfer transfer){
-
-    // }
 }
