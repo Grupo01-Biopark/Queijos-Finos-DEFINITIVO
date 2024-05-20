e296395c007cc2878946711f8f89c11b85e2f0ca
Matheus Morilha <matheusmorilha04@gmail.com>
2024-05-20 20:05:53 -0300

    Convertendo os tipos de produção de String para INT para armazenar no banco de dados como números inteiros.

--- src/main/java/com/example/demo/entity/enums/TipoStatusProduction.java
+++ src/main/java/com/example/demo/entity/enums/TipoStatusProduction.java
@@ -1,8 +1,28 @@
 package com.example.demo.entity.enums;
 
 public enum TipoStatusProduction {
-    PRODUCING, // PRODUZINDO
-    COMPLEMENTATION, // ATIVO EM COMPLEMENTACAO
-    WITHDRAWAL, // DESISTENTE
-    DISCONNECTED; // DESLLIGADO
+    PRODUCING(1), // PRODUZINDO
+    COMPLEMENTATION(2), // ATIVO EM COMPLEMENTACAO
+    WITHDRAWAL(3), // DESISTENTE
+    DISCONNECTED(4); // DESLIGADO
+
+    private final int value;
+
+    TipoStatusProduction(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static TipoStatusProduction fromValue(int value) {
+        for (TipoStatusProduction status : values()) {
+            if (status.getValue() == value) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Invalid value: " + value);
+    }
 }
+
