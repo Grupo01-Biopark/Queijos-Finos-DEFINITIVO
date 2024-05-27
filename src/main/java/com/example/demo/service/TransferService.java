7bfd32c4a23e8a27fa6ef6ac63dac039351959e5
Matheus Morilha <matheusmorilha04@gmail.com>
2024-05-27 20:38:41 -0300

    Implementando a funcionalidade de editar transferencias

--- src/main/java/com/example/demo/service/TransferService.java
+++ src/main/java/com/example/demo/service/TransferService.java
@@ -1,13 +1,14 @@
 package com.example.demo.service;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import com.example.demo.entity.Transfer;
 import com.example.demo.repository.TransferRepository;
-
-
 
 @Service
 public class TransferService {
@@ -16,13 +17,34 @@ public class TransferService {
     private TransferRepository transferRepository;
 
     public void createTransfer(Transfer transfer) {
-        // Lógica para validar e salvar a transferência no banco de dados
         transferRepository.save(transfer);
     }
 
+    @Validated
+    public Transfer updateTransfer(Transfer updateTransfer) {
+        if (updateTransfer.getId() == null) {
+            throw new IllegalArgumentException("ID da transferência não fornecido no objeto atualizado.");
+        }
+
+        Optional<Transfer> optionalTransfer = transferRepository.findById(updateTransfer.getId());
+
+        if (optionalTransfer.isPresent()) {
+            Transfer existingTransfer = optionalTransfer.get();
+            existingTransfer.setTechnology(updateTransfer.getTechnology());
+            existingTransfer.setTipoStatusProduction(updateTransfer.getTipoStatusProduction());
+            existingTransfer.setStartDate(updateTransfer.getStartDate());
+            existingTransfer.setCompletionDate(updateTransfer.getCompletionDate());
+            return transferRepository.save(existingTransfer);
+        } else {
+            throw new IllegalArgumentException("Transfer não encontrada com o ID fornecido: " + updateTransfer.getId());
+        }
+    }
 
     public List<Transfer> getAllTransfers() {
         return transferRepository.findAll();
     }
 
+    public Transfer getTransferById(Long id) {
+        return transferRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Transferência não encontrada"));
+    }
 }
---
+++ src/main/resources/static/script/editTransfer.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const editButtons = document.querySelectorAll(".editButton");
+
+    editButtons.forEach((button) => {
+        button.addEventListener("click", function() {
+            const transferId = this.closest('tr').querySelector('td:first-child').textContent;
+            fetch(`/transfer/edit/${transferId}`)
+                .then(response => response.text())
+                .then(html => {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(html, 'text/html');
+                    const formSection = doc.querySelector('#sectionForm');
+                    document.querySelector('#sectionForm').innerHTML = formSection.innerHTML;
+                    openFormEdit();
+                });
+        });
+    });
+});
+
+function openFormEdit() {
+    const sectionForm = document.getElementById("sectionForm");
+    const buttonAdd = document.getElementById("buttonAdd");
+    const titleForm = document.querySelector(".titleForm");
+    const formAction = sectionForm.querySelector("form");
+
+    formAction.setAttribute("method", "post");
+    formAction.setAttribute("action", "/transfer/updateTransfer");
+    titleForm.textContent = "Atualizar Transferência";
+
+    sectionForm.style.display = "block";
+    buttonAdd.style.display = "none";
+}
